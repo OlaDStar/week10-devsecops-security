375346f0ccff456f37015f4058ba1245c26bc05c
OlaDStar <peterblessing533@gmail.com>
2026-08-04 22:18:36 +0100

    Add fake secrets for Gitleaks testing

---
+++ app/config.py
@@ -0,0 +1,7 @@
+# Vulnerable configuration file
+
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+
+DATABASE_PASSWORD = "SuperSecretPassword123"
